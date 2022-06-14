ea4fdca6f747aa7245d9a1cb0805d2d952bb911b
Wilton Zanin <wiltonczanin@gmail.com>
2022-06-13 21:58:47 -0300

    Implementação do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:video_recorder/camera_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import './galeria.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     title: 'Navegação Básica',
     theme: ThemeData(primarySwatch: Colors.blue),
